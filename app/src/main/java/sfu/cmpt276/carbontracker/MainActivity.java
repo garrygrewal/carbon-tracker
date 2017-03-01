b852a8f687196a5f83fb6bf06d6a334d4b71ccfd
garry <garryg@sfu.ca>
2017-03-01 14:57:57 -0800

    Initial commit of empty project

---
+++ app/src/main/java/sfu/cmpt276/carbontracker/MainActivity.java
@@ -0,0 +1,14 @@
+package sfu.cmpt276.carbontracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //empty project
+    }
+}
